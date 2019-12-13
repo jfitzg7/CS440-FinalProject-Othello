31c18ab9373a6f43cbc6f867df64b784cd78ecf7
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-12-12 18:20:27 -0700

    Added who the winner was to statistics section

--- frontend/othello/src/components/Game.vue
+++ frontend/othello/src/components/Game.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="game">
     <h4 class="text-center"><b> Game Statistics </b></h4>
-    <b-container>
-      <b-list-group>
+    <b-row align-h="center">
+      <b-list-group horizontal="md">
         <b-list-group-item>Number of moves made: {{statistics.numTurns}}</b-list-group-item>
         <b-list-group-item>Number of black pieces: {{statistics.numBlack}}</b-list-group-item>
         <b-list-group-item>Number of white pieces: {{statistics.numWhite}}</b-list-group-item>
+        <b-list-group-item v-if="winner === 'B'">Winner: Black</b-list-group-item>
+        <b-list-group-item v-else-if="winner === 'W'">Winner: White</b-list-group-item>
+        <b-list-group-item v-else>Tie game!</b-list-group-item>
       </b-list-group>
-    </b-container>
+    </b-row>
     <Board></Board>
   </div>
 </template>
@@ -23,7 +26,8 @@
     },
     data() {
       return {
-        statistics: json.statistics
+        statistics: json.statistics,
+        winner: json.winner
       }
     },
     methods: {
