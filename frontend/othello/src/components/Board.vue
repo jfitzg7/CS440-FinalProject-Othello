b1010b96583f42b87d2752b8ef8a9e77189c9dfc
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-12-12 17:43:23 -0700

    Created basic page for displaying game statistics from OthelloEngine json output

--- frontend/othello/src/components/Board.vue
+++ frontend/othello/src/components/Board.vue
@@ -1,9 +1,19 @@
 <template>
+  <b-container class="board">
+    <b-table striped hover :items="items"></b-table>
+  </b-container>
 </template>
 
 <script>
+  import json from "../../../../output.json"
+
   export default {
-    name : 'Board'
+    name : 'Board',
+    data() {
+      return {
+        items: json.turns
+      }
+    }
   }
 </script>
 
--- frontend/othello/src/components/Game.vue
+++ frontend/othello/src/components/Game.vue
@@ -1,12 +1,33 @@
 <template>
-
+  <div class="game">
+    <h4 class="text-center"><b> Game Statistics </b></h4>
+    <b-container>
+      <b-list-group>
+        <b-list-group-item>Number of moves made: {{statistics.numTurns}}</b-list-group-item>
+        <b-list-group-item>Number of black pieces: {{statistics.numBlack}}</b-list-group-item>
+        <b-list-group-item>Number of white pieces: {{statistics.numWhite}}</b-list-group-item>
+      </b-list-group>
+    </b-container>
+    <Board></Board>
+  </div>
 </template>
 
 <script>
+  import json from '../../../../output.json'
   import Board from './Board.vue'
 
   export default {
-    name: 'Game'
+    name: 'Game',
+    components : {
+      Board
+    },
+    data() {
+      return {
+        statistics: json.statistics
+      }
+    },
+    methods: {
+    }
   }
 </script>
 
