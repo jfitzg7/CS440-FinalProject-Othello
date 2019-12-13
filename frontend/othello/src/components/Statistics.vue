a4a1d3bcaecc769ea5b5e2cb019e1473e43689ef
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-12-12 21:59:42 -0700

    Small changes to statistics section

--- frontend/othello/src/components/Statistics.vue
+++ frontend/othello/src/components/Statistics.vue
@@ -3,9 +3,9 @@
     <h4 class="text-center"><b> Game Statistics </b></h4>
     <b-row align-h="center">
       <b-list-group horizontal="md">
-        <b-list-group-item>Number of moves made: {{statistics.numTurns}}</b-list-group-item>
-        <b-list-group-item>Number of black pieces: {{statistics.numBlack}}</b-list-group-item>
-        <b-list-group-item>Number of white pieces: {{statistics.numWhite}}</b-list-group-item>
+        <b-list-group-item>Total moves made: {{statistics.numTurns}}</b-list-group-item>
+        <b-list-group-item>Black pieces: {{statistics.numBlack}}</b-list-group-item>
+        <b-list-group-item>White pieces: {{statistics.numWhite}}</b-list-group-item>
         <b-list-group-item v-if="winner === 'B'">Winner: Black</b-list-group-item>
         <b-list-group-item v-else-if="winner === 'W'">Winner: White</b-list-group-item>
         <b-list-group-item v-else>Tie game!</b-list-group-item>
